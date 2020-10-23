8d46dfe747914ceb2f994bd86c91c2e4205e7136
Meyke98 <meykevannysaptenno@gmail.com>
2020-10-23 22:54:11 +0700

    CRUD Done Paten

--- app/src/main/java/meyke/example/myfirstapp/CRUD/MainMhsActivity.java
+++ app/src/main/java/meyke/example/myfirstapp/CRUD/MainMhsActivity.java
@@ -1,7 +1,10 @@
 package meyke.example.myfirstapp.CRUD;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import meyke.example.myfirstapp.R;
 
@@ -11,5 +14,42 @@ public class MainMhsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_mhs);
+
+        Button btnGetMhs = (Button)findViewById(R.id.btnGetMhs);
+        Button btnAddMhs = (Button)findViewById(R.id.btnAddMhs);
+        Button btnDlt = (Button)findViewById(R.id.btnHapus);
+        Button btnUpdate = (Button)findViewById(R.id.btnUpdate);
+
+        btnGetMhs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMhsActivity.this, MahasiswaGetAllActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnAddMhs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMhsActivity.this, MahasiswaAddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDlt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMhsActivity.this, HapusMhsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMhsActivity.this, MahasiswaUpdateActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
